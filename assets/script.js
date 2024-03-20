4c61d4710229e6d4c87d26292317f1b8a365c009
jorgenettodev <jorgenetto96@gmail.com>
2024-03-20 19:49:47 -0300

    Base of the gameboard object and gamecontroller obj

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
 console.log('tic tac toe project');
+
+const Gameboard = function() {
+    // create an array to be used as gameboard
+    /*
+    * 0 0 0
+    * 0 0 0
+    * 0 0 0
+    */
+}
+
+const gameController = function() {
+    const player1 = {name: "Player 1", token: "X"};
+    const player2 = {name: "Player 2", token: "O"};
+}
