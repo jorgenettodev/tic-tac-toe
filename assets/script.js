6092560b486369dd7d4672329344136ea14be80a
jorgenettodev <jorgenetto96@gmail.com>
2024-03-21 19:22:56 -0300

    Create the board logic

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,39 @@
 console.log('tic tac toe project');
 
-const Gameboard = function() {
-    // create an array to be used as gameboard
-    /*
-    * 0 0 0
-    * 0 0 0
-    * 0 0 0
-    */
+const Cell = function() {
+    let value = 0;
+
+    const addToken = function(row, col, player) {
+        value = player.token;
+    }
+
+    const getValue = () => value;
+
+    return { addToken, getValue };
 }
 
-const gameController = function() {
+const Gameboard = (function() {
+    let board = [];
+    const rows = 3;
+    const columns = 3;
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            let cell = Cell();
+            board[i].push(cell);
+        }
+    }
+
+    const getBoard = () => board;
+
+   return { getBoard };
+})();
+
+const GameController = function() {
     const player1 = {name: "Player 1", token: "X"};
     const player2 = {name: "Player 2", token: "O"};
 }
+
+
+console.log(Gameboard.getBoard());
